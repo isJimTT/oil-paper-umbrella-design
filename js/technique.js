d780db1526e81fd17650051f4f6ec19d48cc68a2
LaaoG <1656615127@qq.com>
2024-05-29 21:05:36 +0800

    技艺页面第一次提交

---
+++ js/technique.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 50,
+  loop: true,
+  // freeMode: true,
+  autoplay: {
+    delay: 2500,
+    // disableOnInteraction: true,
+  },
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+window.addEventListener("scroll", () => {
+  const element = document.querySelector(".mengban");
+  const scrollPosition = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const documentHeight = document.body.clientHeight;
+  // 计算元素距离底部的距离
+  const elementOffset = documentHeight - (scrollPosition + windowHeight);
+
+  if (elementOffset < 0.2 * windowHeight) {
+    element.style.bottom = "-10vw";
+  } else {
+    element.style.bottom = "0";
+  }
+});
