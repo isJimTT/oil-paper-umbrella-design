b1c3826c8087e661897fa223318b00321b0421bc
Jim__TT <1820452084@qq.com>
2024-06-13 04:09:53 +0800

    feat: 终版V1.1

--- js/home.js
+++ js/home.js
@@ -3,3 +3,23 @@ let title = document.querySelector(".title img");
 title.onclick = function () {
   window.location = "./view/directory.html";
 };
+
+document.querySelector(".title img").addEventListener("mouseover", function () {
+  document.querySelector(".umbrella-title-left").classList.add("shanks-left");
+});
+
+document.querySelector(".title img").addEventListener("mouseout", function () {
+  document
+    .querySelector(".umbrella-title-left")
+    .classList.remove("shanks-left");
+});
+
+document.querySelector(".title img").addEventListener("mouseover", function () {
+  document.querySelector(".umbrella-title-right").classList.add("shanks-right");
+});
+
+document.querySelector(".title img").addEventListener("mouseout", function () {
+  document
+    .querySelector(".umbrella-title-right")
+    .classList.remove("shanks-right");
+});
